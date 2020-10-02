439374a7169d93e1ef77d9870ad5f9fd39951acb
USER <USER@DESKTOP-29QUDS2>
2020-10-02 09:03:11 -0500

    hola

--- resources/views/registros.blade.php
+++ resources/views/registros.blade.php
@@ -35,7 +35,16 @@
                             <td>{{$val->apellido}}</td>
                             <td>{{$val->nacimiento}}</td>
                             <td>{{$val->direccion}}</td>
-                            <td><a class="btn-warning" href="{{route('home.edit',$val->id )}}" ><i class="fa fa-edit"></i></a></td>
+                            <td><button class="btn-warning" data-toggle='modal' data-target='#formEditar'
+                                        onclik="mostrarDatos(this);"
+                                        data-id="{{$val->id}}" data-nombre="{{$val->nombre}}" 
+                                        data-ap="{{$val->apellido}}" data-na="{{$val->nacimiento}}"
+                                        data-dir="{{$val->direccion}}">
+                                    
+                                    <i class="fa fa-edit"></i>
+                                    
+                                </button>
+                            </td>
                             <td></td>
                             
                         </tr>
@@ -112,4 +121,86 @@
      
     </div>
 
+
+
+
+<div class="modal fade" id="formEditar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Editar Registro</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+              <form method="POST" action="{{route('home.store')}}">     
+                  @csrf
+            <div class="row">
+                    <div class="col-md-4">
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                              <span class="input-group-text" id="basic-addon1"><i class="fa fa-user"></i></span>
+                            </div>
+                            <input required type="text" class="form-control" placeholder="Nombre" id="enombre" name="enombre">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="basic-addon1"><i class="fa fa-users"></i></span>
+                            </div>
+                            <input required type="text" class="form-control" placeholder="Apellidos" id="eapellido" name="eapellido">
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="basic-addon1"><i class="fa fa-birthday-cake"></i></span>
+                            </div>
+                            <input required type="date" class="form-control" placeholder="Fecha" id="eedad" name="eedad">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="input-group mb-3">
+                            <div class="input-group-prepend">
+                                <span class="input-group-text" id="basic-addon1"><i class="fa fa-building"></i></span>
+                            </div>
+                            <textarea required id="edom" name="edom" class="form-control" rows="1" placeholder="Domicilio"></textarea>
+                        </div>
+                    </div>
+                </div>
+              <div class="row">
+                  <div class="col-md-4 offset-md-4">
+                      <button type="submit" class="btn btn-success" id="eguardar" ><i class="fa fa-sync-alt"></i> EDITAR</button>
+                  </div>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal" id="btnCerrar">Cerrar</button>
+          </div>
+        </div>
+      </div>
+     
+    </div>
+
+
+<script>
+    function mostrarDatos(btn){
+        var nombre =$(btn).attr("data-nombre");
+        var ap =$(btn).attr("data-ap");
+        var na =$(btn).attr("data-na");
+        var dir =$(btn).attr("data-dir");
+        
+        $("#enombre").val(nombre);
+        $("#eapellido").val(ap);
+        $("#eedad").val(na);
+        $("#edom").val(dir);
+    }
+
+
+</script>
 @endsection
